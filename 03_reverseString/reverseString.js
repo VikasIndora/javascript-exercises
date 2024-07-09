49f82ba66add7ba2273d6002c6ad6a7fe68a0f5f
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-09 23:14:51 +0530

    Solve 1,2 and 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordIntoArray = word.split('');
+    let reversedArray = wordIntoArray.reverse();
+    return reversedArray.join('');
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    for(let i = 0; i < num2; i++){
+        let nextNum = arr[arr.length - 1] += 1;
+        arr.push(nextNum);
+    }
 };
 
 // Do not edit below this line
