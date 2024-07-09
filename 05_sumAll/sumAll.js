845411dde407d09226c2e6da98438f799f540429
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-10 00:03:19 +0530

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,22 @@
-const sumAll = function(num1,num2) {
-    for(let i = 0; i < num2; i++){
-        let nextNum = arr[arr.length - 1] += 1;
-        arr.push(nextNum);
+const sumAll = function(num1, num2) {
+    if (num1 > num2){
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    } else if (num1 < 0 || num2 < 0){
+        return "ERROR"
+    } else if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return "ERROR"
     }
+    const arr = []
+    for(num1; num1 <= num2; num1++){
+        arr.push(num1)
+    }
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    return sum;
 };
 
 // Do not edit below this line
