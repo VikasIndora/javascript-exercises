110a336545d8f38ccf1ace3bdcff6807e54049a5
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-10 18:26:19 +0530

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1 , num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr == []) return 0;
+	let addition = 0;
+  for(let i = 0; i < arr.length; i++){
+    addition += arr[i];
+  }
+  return addition;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr == []) return 0;
+	let product = 1;
+  for(let i = 0; i < arr.length; i++){
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num,toPower) {
+	return num ** toPower;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  if (num == 0 || num == 1) return 1;
+	for (num; num >= 1; num--){
+    product *= num;
+  }
+  return product;
 };
 
 // Do not edit below this line
