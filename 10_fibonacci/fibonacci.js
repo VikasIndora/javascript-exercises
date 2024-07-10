8a8a4bd066a569f338883e5035223804bba72fc5
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-10 20:52:06 +0530

    Solve 10
    Yay!! feeling better than previous commit.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    +index;
+    if (index < 0) return 'OOPS'
+    let fibArray = [];
+    fibArray[0] = 0;
+    fibArray[1] = 1;
+    for(let i=2; i<=index; i++){
+        fibArray[i] = fibArray[i-1] + fibArray[i-2];
+    }
+    return fibArray[index];
 };
 
 // Do not edit below this line
