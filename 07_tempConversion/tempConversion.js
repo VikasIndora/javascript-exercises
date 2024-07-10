5edfa3fcd632a58761f2427e9e554dabfc49d0ee
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-10 17:46:58 +0530

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+const convertToCelsius = function(fahrenheit) {
+  let number = (fahrenheit - 32) * (5/9);
+  return round(number,1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let number = celsius * (9/5) + 32;
+  return round(number,1);
 };
 
 // Do not edit below this line
