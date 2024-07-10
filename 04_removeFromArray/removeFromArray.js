02f28fbf49063277f75dfa56d9aea4714d39b0eb
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-10 20:02:17 +0530

    Try 09 and 04
    I am dumb bro. passed 6/8 checks for 09 but that too with a badly written function I feel.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...element) {
+    let indexOfElement = -1;
+    for(let i = 0; i<array.length; i++){
+        if(array[i] == element){
+            indexOfElement = i;
+            break;
+        }
+    }
+    if (indexOfElement !== -1){
+        array.splice(indexOfElement,1);
+    }
+    return array;
 };
-
+// FUCK THIS EXERCISE AND ITS TEST CASES and fuck problem solving if I don't even have the pre-requisites which the solution wants me to work with. "SeArCh fOr tHeM" my ass.
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (something) {
+    something = something.trim();
+    let punctuation;
+    let newArray = something.split('');
+    if (newArray[newArray.length-1] == '!' || newArray[newArray.length-1] == '.'){
+        punctuation = newArray.splice(newArray.length-1,1)
+    }
+    let reversedArray = newArray.reverse();
+    let newSomething = reversedArray.join('');
+    let originalWithoutPunctuation = newArray.join('');
+    if (originalWithoutPunctuation.toLowerCase() === newSomething.toLowerCase()){
+        return true;
+    }else return false;
 };
 
 // Do not edit below this line
