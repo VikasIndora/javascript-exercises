26ebb894b274bfe79b2d1ec88b56d94764f0c368
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-11 16:49:31 +0530

    Solve 11
    It feels weird cause I am solving these but my solution doesn't match with their's, BUT its passing all the tests so ig its fine??

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,18 @@
-const getTheTitles = function() {
+// const books = [
+//     {
+//       title: 'Think like a programmer',
+//       author: 'V Anton Spraul'
+//     },
+//     {
+//       title: 'Atomic Habits',
+//       author: 'James Clear'
+//     }
+//   ]
+const getTheTitles = function(books) {
 
+    titleArray = [books[0].title,books[1].title]
+    return titleArray;
+    
 };
 
 // Do not edit below this line
