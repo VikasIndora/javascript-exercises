37aca853f016f403ba76c93a336edabceeadc7a5
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-12 16:16:30 +0530

    Solve 09
    Finally all done. Key was to learn about crazy array and string methods.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,16 +1,18 @@
-const palindromes = function (something) {
-    something = something.trim();
-    let punctuation;
-    let newArray = something.split('');
-    if (newArray[newArray.length-1] == '!' || newArray[newArray.length-1] == '.'){
-        punctuation = newArray.splice(newArray.length-1,1)
-    }
-    let reversedArray = newArray.reverse();
-    let newSomething = reversedArray.join('');
-    let originalWithoutPunctuation = newArray.join('');
-    if (originalWithoutPunctuation.toLowerCase() === newSomething.toLowerCase()){
-        return true;
-    }else return false;
+const palindromes = function (input) {
+    let alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let cleanedInput = input
+    .trim()
+    .toLowerCase()
+    .split('')
+    .filter( (i) => alphanumerical.includes(i) )
+    .join('');
+
+    let reversedInput = cleanedInput
+    .split('')
+    .reverse()
+    .join('');
+
+    return cleanedInput === reversedInput;
 };
 
 // Do not edit below this line
