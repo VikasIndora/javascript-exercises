09620842d94bdff66724056c73590653d6786b34
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-12 16:04:52 +0530

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,9 @@
-const removeFromArray = function(array,...element) {
-    let indexOfElement = -1;
-    for(let i = 0; i<array.length; i++){
-        if(array[i] == element){
-            indexOfElement = i;
-            break;
-        }
-    }
-    if (indexOfElement !== -1){
-        array.splice(indexOfElement,1);
-    }
-    return array;
+const removeFromArray = function(array, ...args) {
+    let filteredArray = array.filter( (items) => !args.includes(items) )
+    return filteredArray;
 };
 // FUCK THIS EXERCISE AND ITS TEST CASES and fuck problem solving if I don't even have the pre-requisites which the solution wants me to work with. "SeArCh fOr tHeM" my ass.
+// learned about some array methods and did it again.
+
 // Do not edit below this line
 module.exports = removeFromArray;
